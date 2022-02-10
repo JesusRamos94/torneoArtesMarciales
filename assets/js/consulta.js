e817e0fa93fb326caad2366374c54c498d7ead3a
Jesus Ramos <jeenrade@gmail.com>
2022-02-10 12:17:35 -0300

    Creando el modulo consultas

---
+++ assets/js/consulta.js
@@ -0,0 +1,12 @@
+let personajes = (() => {
+    const url = "http://127.0.0.1:5502/dbz.json";
+    const getData = async () => {
+        const res = await fetch(url)
+        const data = await res.json();
+        return data;
+    };
+
+    return {getData};
+})();
+
+export default personajes;
